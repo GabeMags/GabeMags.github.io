7f3a4a878d8e2a456812b0da0bd12ca3d9735f53
GabeMags <magallanes.gabe@gmail.com>
2021-11-23 15:00:30 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 
+// React imports
+import React, { Component } from "react";
+
+// Bootstrap imports
+import "bootswatch/dist/darkly/bootstrap.min.css";
+// import 'jquery/dist/jquery.min.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+// Components
+import MyNavbar from "./components/Navbar";
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid={true} className="pt-2">
+        <MyNavbar />
+    </Container>
   );
 }
 
